959f2b6960567dc2cbb5176109458ff2af21742b
IhorKostenko <pmkostenkoihor@gmail.com>
2021-12-01 22:52:18 +0200

    version 5.1

---
+++ js/filter.js
@@ -0,0 +1,44 @@
+const menupfolio = document.querySelector('.menu-pfolio'),
+    items = document.querySelectorAll('.table_block')
+    listitems = document.querySelectorAll('.navlink-pfolio')
+
+function filter() {
+    menupfolio.addEventListener('click', event => {
+        event.preventDefault();
+        const targetID = event.target.dataset.id 
+        console.log(targetID)
+
+        listitems.forEach(listitem => listitem.classList.remove('active'))
+
+        switch (targetID) {
+            case 'all':
+                getItems('table_block')
+                break
+            case 'website':
+                getItems(targetID)
+                break
+            case 'apps':
+                getItems(targetID)
+                break
+            case 'design':
+                getItems(targetID)
+                break
+            case 'market':
+                getItems(targetID)
+                break
+
+        }
+   }) 
+}
+filter()
+
+function getItems(className) {
+    items.forEach(item => {
+                    if (item.classList.contains(className)) {
+                        item.style.display = 'block'
+                    }
+                    else {
+                        item.style.display = 'none'
+                    }
+                })
+}
